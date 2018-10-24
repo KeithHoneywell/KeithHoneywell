b0db16340efbb48dbbc8db33223853efcb7adceb
KeithHoneywell <44329396+KeithHoneywell@users.noreply.github.com>
2018-10-23 21:46:57 -0700

    Add files via upload

---
+++ Lists1.py
@@ -0,0 +1,19 @@
+days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday" ]
+print(days_of_week[0])
+print(days_of_week[3])
+print(
+
+)
+print(days_of_week[-1])
+print(days_of_week[-2])
+print(
+
+)
+print(days_of_week[2:])
+print(days_of_week[1:3])
+print(
+
+)
+print("\"How to Modify a value in a list\"")
+days_of_week[1] = "Twosday"
+print(days_of_week[1])
